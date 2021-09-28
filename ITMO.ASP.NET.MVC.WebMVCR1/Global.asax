5f7393254725791a8ee59d8b5639304d0f6d3dfe
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-09-28 21:41:13 +0300

    Add project files.

---
+++ ITMO.ASP.NET.MVC.WebMVCR1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.ASP.NET.MVC.WebMVCR1.MvcApplication" Language="C#" %>
---
+++ ITMO.ASP.NET.MVC.WebMVCR1/Models/ModelClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET.MVC.WebMVCR1.Models
+{
+    public class ModelClass
+    {
+    }
+}
