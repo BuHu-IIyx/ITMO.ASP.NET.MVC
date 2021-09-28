a48e4b4df64edbfaa90ed4c675b51545c1d55697
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-09-28 21:41:57 +0300

    Lab1 ex2

--- ITMO.ASP.NET.MVC.WebMVCR1/Controllers/HomeController.cs
+++ ITMO.ASP.NET.MVC.WebMVCR1/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace ITMO.ASP.NET.MVC.WebMVCR1.Controllers
             int hour = DateTime.Now.Hour;
             string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel;
             return Greeting;
+
         }
     }
 }
